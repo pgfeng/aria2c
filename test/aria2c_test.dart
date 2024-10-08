f84fc9026cb86881431afe298215ccf707f4f446
PGF <228375325@qq.com>
2024-10-08 17:08:40 +0800

    Commit

--- test/aria2c_test.dart
+++ test/aria2c_test.dart
@@ -10,11 +10,6 @@ void main() {
   test('Test SocketConnect', () async {
     Aria2cWebSocketRPC rpc = Aria2cWebSocketRPC(Uri.parse('ws://127.0.0.1:6800/jsonrpc'), rpcSecret: '123456789');
     await rpc.connect();
-    // rpc.messages.listen((r){
-    //   print('???');
-    //   print(r);
-    //   print(r.runtimeType);
-    // });
     print('Socket Connected');
     rpc.getVersion().then((r){
       print(r);
@@ -23,14 +18,14 @@ void main() {
       print(r);
     });
   });
-  // test('Test Aria2cStatus FromJson', () async {
-  //   print(Aria2cStatus.fromJson({
-  //     "gid": "0",
-  //     "status": "active",
-  //     "totalLength": 0,
-  //     "completedLength": 0,
-  //     "uploadLength": 0,
-  //     "bitfield": "000000000000000000000000000",
-  //   }));
-  // });
+  test('Test Aria2cStatus FromJson', () async {
+    print(Aria2cStatusResult.fromJson({
+      "gid": "0",
+      "status": "active",
+      "totalLength": 0,
+      "completedLength": 0,
+      "uploadLength": 0,
+      "bitfield": "000000000000000000000000000",
+    }));
+  });
 }
